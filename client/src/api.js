9b91c4ef6bcb15bc11603cb5789b404f93b49aa7
Alfred Ochieng <techyobura@gmail.com>
2025-01-22 23:54:34 +0300

    second

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 const local = "http://localhost:7000";
-const render = "https://portfolio2-copy.onrender.com";
-const api = `${local}/api`;
+const render = "https://blog-xdb7.onrender.com";
+const api = `${render}/api`;
 
 export default api;
